280b514126042a0c227f4a0c29b5faaeb4f6305e
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-10-17 15:22:51 -0600

    update coding challenge 1

--- starter/script.js
+++ starter/script.js
@@ -17,7 +17,7 @@ const renderCountry = function (data, className) {
     </div>
   </article> `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  //moved to getCountryData2// countriesContainer.style.opacity = 1; //set the image to visible
+  countriesContainer.style.opacity = 1; //set the image to visible //moved to getCountryData2
 };
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
@@ -146,16 +146,16 @@ const renderError = function (msg) {
 ////   countriesContainer.insertAdjacentText('beforeend', msg);
 ////   countriesContainer.style.opacity = 1;
 //// };
-const getJSON = function (url, errorMsg = 'something went wrong') {
-  return fetch(url).then(response => {
-    console.log(response);
-    if (!response.ok) {
-      //this is being passed to the catch method
-      throw new Error(`${errorMsg} (${response.status})`);
-    }
-    return response.json();
-  });
-};
+// const getJSON = function (url, errorMsg = 'something went wrong') {
+//   return fetch(url).then(response => {
+//     console.log(response);
+//     if (!response.ok) {
+//       //this is being passed to the catch method
+//       throw new Error(`${errorMsg} (${response.status})`);
+//     }
+//     return response.json();
+//   });
+// };
 // const getCountryData2 = function (country) {
 //   //country 1
 
@@ -207,49 +207,100 @@ const getJSON = function (url, errorMsg = 'something went wrong') {
 //or a container for a future value
 
 //!======================
-const getCountryData3 = function (country) {
-  //country 1
-  //the getJSON function gets the url and error message making the code block much cleaner to prevent repeating ourselves
-  getJSON(`https://restcountries.com/v2/name/${country}`, 'country  not found')
-    //// fetch(`https://restcountries.com/v2/name/${country}`) //fetch the  data
-    ////   .then(response => {
-    ////     console.log(response);
-    ////     if (!response.ok) {
-    //       //this is being passed to the catch method
-    ////       throw new Error(`country not found (${response.status})`);
-    ////     }
-    ////     return response.json();
-    ////   })
-    //take response the put it in json to be put together
+// const getCountryData3 = function (country) {
+//country 1
+//the getJSON function gets the url and error message making the code block much cleaner to prevent repeating ourselves
+// getJSON(`https://restcountries.com/v2/name/${country}`, 'country  not found')
+//// fetch(`https://restcountries.com/v2/name/${country}`) //fetch the  data
+////   .then(response => {
+////     console.log(response);
+////     if (!response.ok) {
+//       //this is being passed to the catch method
+////       throw new Error(`country not found (${response.status})`);
+////     }
+////     return response.json();
+////   })
+// take response the put it in json to be put together
+//     .then(data => {
+//       renderCountry(data[0]); //then render the country data
+//       const neighbour = data[0].borders[0];
+//       if (!neighbour) throw new Error('No neighbour found!');
+//       console.log(neighbour);
+
+//       //country 2
+//       return getJSON(
+//         `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+//         'country not found'
+//       );
+//       // return 23;
+//     })
+//     //// .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'))
+//     //catch the error from the api not fetching
+//     .catch(err => {
+//       //using catch to catch failed promises is a good practice
+//       //catching the fetch api error
+//       console.error(`${err}💥💥💥`);
+//       renderError(`Something went wrong 💥💥💥${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+
+//   // .then(data => alert(23));
+//   //get rid of call back hell
+//   //the two then methods are a chain of promises
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountryData3('australia');
+// });
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=xml&auth=your_api_key`)
+    .then(res => {
+      if (!res.ok) throw new Error(`problem with geocoding ${res.status}`);
+      return res.json();
+    })
     .then(data => {
-      renderCountry(data[0]); //then render the country data
-      const neighbour = data[0].borders[0];
-      if (!neighbour) throw new Error('No neighbour found!');
-      //country 2
-      return getJSON(
-        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
-        'country not found'
-      );
-      // return 23;
-    })
-    //// .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'))
-    //catch the error from the api not fetching
-    .catch(err => {
-      //using catch to catch failed promises is a good practice
-      //catching the fetch api error
-      console.error(`${err}💥💥💥`);
-      renderError(`Something went wrong 💥💥💥${err.message}. Try again!`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+      console.log(data);
+      console.log(`You are in ${data.city},${data.country}`);
 
-  // .then(data => alert(23));
-  //get rid of call back hell
-  //the two then methods are a chain of promises
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message}💥`));
 };
-
-btn.addEventListener('click', function () {
-  getCountryData3('australia');
-});
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
