92e32838b5b11290dde83a998936f26d878076bd
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-10-18 14:40:25 -0600

    update part 2

--- starter/script.js
+++ starter/script.js
@@ -688,44 +688,54 @@ const createImage = function (imgPath) {
   });
 };
 
-let currentImg;
-createImage('img/img-1.jpg')
-  .then(img => {
-    currentImg = img;
-    console.log('image 1 loaded');
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-    return createImage('img/img-2.jpg');
-  })
-  .then(img => {
-    currentImg = img;
-    console.log('image 2 loaded');
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-    return createImage('img/img-3.jpg');
-  })
-  .catch(err => console.log(err));
+// let currentImg;
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-3.jpg');
+//   })
+//   .catch(err => console.log(err));
+//!=======
+// part 1
+const loadNPause = async function () {
+  try {
+    //load img 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
 
-  const loadNPause = async function(){
-    try {
-      //load img 1
-     let img = await createImage('img/img-1.jpg')
-     console.log('Image 1 loaded');
-     await wait(2);
-     img.stlye.display ='none'
-
-      //load img 2
-     img = await createImage('img/img-1.jpg')
-      console.log('Image 1 loaded');
-      await wait(2);
-      img.stlye.display ='none'
-
-    } catch (err) {
-      console.error(err);
-    }
-    }
+    //load img 2
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
   }
+};
+loadNPause();
+
+//Part 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs2 = imgArr.map(async img => await createImage(img));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
