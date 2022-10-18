ed8b8533e08c5223cf01c1456685749128429901
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-10-18 10:34:24 -0600

    coding challenge 2

--- starter/script.js
+++ starter/script.js
@@ -316,39 +316,39 @@ GOOD LUCK 😀
 // console.log('Test end');
 //!========================
 //------ Building a Simple Promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log('Lotter draw is happening');
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve('YOU WIN 💰');
-    } else {
-      reject(new Error('You lost your money 💥'));
-    }
-  }, 2000);
-});
-lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('YOU WIN 💰');
+//     } else {
+//       reject(new Error('You lost your money 💥'));
+//     }
+//   }, 2000);
+// });
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
-//promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-};
+// //promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
 
-wait(1)
-  .then(() => {
-    console.log('I waited for 1 seconds');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('I waited for 2 seconds');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('I waited for 3 seconds');
-    return wait(1);
-  })
-  .then(() => console.log('4 second passed'));
+// wait(1)
+//   .then(() => {
+//     console.log('I waited for 1 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('I waited for 3 seconds');
+//     return wait(1);
+//   })
+//   .then(() => console.log('4 second passed'));
 
 // example of callback within callbacks
 // setTimeout(() => {
@@ -366,3 +366,103 @@ wait(1)
 Promise.resolve('abc').then(x => console.log(x));
 Promise.resolve(new Error('Problem!')).catch(x => console.error(x));
 //!=================================
+// promisify the geolocation API cannot do due to not having an API key
+//get the position of a certain latitude and longitude
+//promise based API
+//using reverse geocoding
+//get position based on the location of the device
+//// const getPosition = function () {
+////   return new Promise(function (resolve, reject) {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   position => resolve(position),
+//     //   err => reject(err)
+//     // );
+////     navigator.geolocation.getCurrentPosition(resolve, reject);
+////   });
+//// };
+// // getPosition().then(pos => console.log(pos));
+
+//// const whereAmI = function () {
+////   getPosition()
+////     .then(pos => {
+////       const { latitude: lat, longitude: lng } = pos.coords;
+
+////       return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+////     })
+////     .then(res => {
+////       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+////       return res.json();
+////     })
+////     .then(data => {
+////       console.log(data);
+////       console.log(`You are in ${data.city}, ${data.country}`);
+
+////      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+////     })
+////     .then(res => {
+////       if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+////       return res.json();
+////     })
+////     .then(data => renderCountry(data[0]))
+////     .catch(err => console.error(`${err.message} 💥`));
+//// };
+
+//// btn.addEventListener('click', whereAmI);
+//!=================================
+//coding challenge #2
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+const imgContainer = document.querySelector('.images');
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    //create a new promise that will contain the image path
+    const img = document.createElement('img'); //create the img element
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    console.log('image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
